589d94652f2a4223b94cdae52138fdfc53abc304
Gultzz <83468422+Gultzz@users.noreply.github.com>
2021-08-19 12:56:23 -0300

    Terceira atualuzação

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function lista(){
     var nada = document.getElementById("nadanada").style;
     if(nada.width == "0px" || nada.width == 0){
-        nada.width = "564px";
+        nada.width = "590px";
         console.log(nada.width);
-    }else if(nada.width == "564px"){
+    }else if(nada.width == "590px"){
         nada.width = "0px";
         console.log(nada.width);
     }
